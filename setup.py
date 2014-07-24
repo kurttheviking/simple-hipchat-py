edd48d5deb9da6a08deda30a470275c5e94485ec
kurttheviking <kurt@agilediagnosis.com>
2014-07-23 20:19:20 -0700

    tick patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Easy peasy wrapper for HipChat's v1 API",
     name='python-simple-hipchat',
     url='https://github.com/kurttheviking/python-simple-hipchat',
-    version='0.3.2',
+    version='0.3.3',
     packages=['hipchat'],
     author='Kurt Ericson',
     author_email='kurttheviking@outlook.com',
